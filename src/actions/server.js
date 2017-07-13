fdf2db258062f498bad7c3d6ff0502fdfd21b2ac
huyueb <1062887235@qq.com>
2017-07-13 09:39:48 +0800

    删除引入不必要的模块，删除注释等

--- src/actions/server.js
+++ src/actions/server.js
@@ -1,8 +1,5 @@
-var Koa = require('koa');
 var fs = require('fs');
 var path = require('path');
-var logger = require('koa-logger');
-var serve = require('koa-static');
 var JSON5 = require('json5');
 var browsersync = require('browser-sync').create();
 
@@ -24,23 +21,6 @@ module.exports = function(cwd) {
     if (config && config.dest) {
         dest = config.dest;
     }
-    // var app = new Koa();
-    //
-    // app.use(async (ctx, next) => {
-    //     if (ctx.request.url === '/') {
-    //         console.log(cwd);
-    //         console.log(path.resolve(cwd, dest + '/index.html'));
-    //         ctx.redirect(dest + '/index.html');
-    //     } else {
-    //         await next();
-    //     }
-    // });
-    //
-    // app.use(logger());
-    // app.use(serve(cwd));
-    //
-    // app.listen(8001);
-    //
     browsersync.init({
         port: 8001,
         startPath: dest+'/index.html',//默认打开的初始地址，可以不加
